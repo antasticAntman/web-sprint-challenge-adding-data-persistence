b7089cfcf363a307031dfd2dfe987442831239d5
Anthony Climer <anthonyclimer0925@gmail.com>
2023-05-10 14:01:39 -0400

    I got the server and the first router almost done

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,10 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function getAll(){
+    return Promise.resolve('hahaha gotcha')
+}
+
+module.exports = {
+    getAll
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,28 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+const Resources = require('./model')
+
+// router.use('*', (req, res, next) => { //eslint-disable-line
+//     res.json({
+//         name:"John Wick",
+//         status:"widowed"
+//     })
+// })
+
+router.get('/', (req, res, next) => {
+    Resources.getAll()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(next)
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        customMessage:"Something is incorrect",
+        message:err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
